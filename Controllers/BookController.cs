6fd9a0fdde36692978bf52eb6244659fd163b236
DESKTOP-QNQCNJ8\sviat <sviatko228@gmail.com>
2023-04-28 12:50:55 +0300

    Controller Added

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,6 +1,31 @@
-﻿namespace Books.Controllers
+﻿using Books.Data;
+using Books.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Books.Controllers
 {
-    public class BookController
+    [Route("api/books")]
+    [ApiController]
+    public class BookController : ControllerBase
     {
+        private readonly IRepository<Book> _repository;
+        public BookController(IRepository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200,Type =typeof(IEnumerable<Book>))]
+        public IActionResult GetBooks()
+        {
+            var books = _repository.GetAll();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(books);
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
+using Books.Data;
+using Books.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository<Book>,BookRepository>();
 
 var app = builder.Build();
 
